7a7150c6f857c773465758578daa45476ae2397c
Nipun Gupta <gupta.nipun99@gmail.com>
2020-07-12 23:29:53 +0530

    create page ui

--- src/pages/create/create.page.js
+++ src/pages/create/create.page.js
@@ -1,32 +1,79 @@
-import React from 'react';
-import HeadingPair from '../../components/heading-pair/heading-pair.component';
-import Sidebar from '../../components/sidebar/sidebar';
-
-
+import React from "react";
+import HeadingPair from "../../components/heading-pair/heading-pair.component";
+import Sidebar from "../../components/sidebar/sidebar";
+import FormInput from "../../components/form-input/form-input.component";
 function CreatePage() {
-
-    return (
-        <div className="container-fluid">
-            <div className="row">
-                <Sidebar />
-                <div className="px-5 border col-md-10 d-md-block ">
-                    <div className="container border" >
-                        <HeadingPair heading="Color Theming" subheading="Select your preferred color theming for invoices" />
-                        <div className="d-flex flex-row justify-content-around m-2 p-4" >
-                            <button value="primary" className="color bg-primary" onClick={e => console.log(e.target.value)} ></button>
-                            <button className="color bg-secondary "></button>
-                            <button className="color bg-success "></button>
-                            <button className="color bg-danger "></button>
-                            <button className="color bg-info "></button>
-                            <button className="color bg-dark "></button>
-                        </div>
-                    </div>
-
-                </div>
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <Sidebar />
+        <div className="px-5 border col-md-10 ">
+          <div className="container border">
+            <HeadingPair
+              heading="Create Invoice"
+              subheading="Enter the invoice details below"
+            />
+            <div className="d-flex flex-row m-2 p-4">
+              <button className="btn btn-dark">Select Existing Customer</button>
             </div>
+            <div className="container-fluid">
+              <h4 className="bg-dark text-center text-white">DETAILS</h4>
+              <div className="d-flex flex-row flex-wrap justify-content-start ">
+                <FormInput label="Name" />
+                <FormInput label="Name" />
+                <FormInput label="Name" />
+                <FormInput label="Name" />
+              </div>
+            </div>
+            <div className="container-fluid">
+              <h4 className="bg-dark text-center text-white">ITEMS</h4>
+              <div className="d-flex flex-row justify-content-start ">
+                <FormInput label="Name" />
+                <FormInput label="Name" />
+                <FormInput label="Name" />
+                <FormInput label="Name" />
+              </div>
+              <div className="d-flex flex-row justify-content-start ">
+                <FormInput label="Name" />
+                <FormInput label="Name" />
+                <FormInput label="Name" />
+                <FormInput label="Name" />
+              </div>
+              <div className="d-flex flex-row justify-content-start ">
+                <FormInput label="Name" />
+                <FormInput label="Name" />
+                <FormInput label="Name" />
+                <FormInput label="Name" />
+              </div>
+              <button className=" text-center btn btn-dark my-3">
+                Add More Item
+              </button>
+            </div>
+            <div className="container-fluid">
+              <h4 className="bg-dark text-center text-white">PREVIEW</h4>
+              <div className="d-flex flex-column px-5 mx-5 justify-content-center ">
+                <div className="d-flex flex-row justify-content-between">
+                  <p>TAXABLE AMOUNT</p>
+                  &nbsp; &nbsp;
+                  <p>40000 </p>
+                </div>
+                <div className="d-flex flex-row justify-content-between">
+                  <p>TAXABLE AMOUNT</p>
+                  &nbsp; &nbsp;
+                  <p>40000 </p>
+                </div>
+                <div className="d-flex flex-row justify-content-between">
+                  <p>TAXABLE AMOUNT</p>
+                  &nbsp; &nbsp;
+                  <p>40000 </p>
+                </div>
+                <button className="btn btn-dark">Create Invoice</button>
+              </div>
+            </div>
+          </div>
         </div>
-
-    )
-
+      </div>
+    </div>
+  );
 }
 export default CreatePage;
